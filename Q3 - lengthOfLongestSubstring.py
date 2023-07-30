5c5dab3a7ba7c4086e98cc44bd95ebaa6e4debb5
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-07-30 11:27:23 +0530

    day 2

---
+++ Q3 - lengthOfLongestSubstring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+                
+        count = ''
+        max_len = 0
+        for char in s:
+            if char in count:
+                chars = chars[chars.index(char) + 1:]
+            else:
+                
+                
+                            
+            return len
+            
