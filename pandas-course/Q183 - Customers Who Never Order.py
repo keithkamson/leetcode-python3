5684ea120be35cd43a765d891613a431a17666a0
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-08-08 21:04:08 +0530

    Update Q183 - Customers Who Never Order.py

--- pandas-course/Q183 - Customers Who Never Order.py
+++ pandas-course/Q183 - Customers Who Never Order.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    #customers.rename(columns={'id':'customerID','name':'Customers'}, inplace = True)
-    #no_order = orders.join(customers, how='outer')
-    #no_order['id'].fillna(0,inplace=True)
-    #no_order_f = no_order[(no_order['id']==0)]
-    #return no_order_f[['Customers']]
-    
-    
+    customers.rename(columns={'id':'customerId','name':'Customers'}, inplace = True)
+    no_order = pd.merge(customers, orders,how="left", on="customerId")
+    no_order['id'].fillna(0,inplace=True)
+    no_order_f = no_order[(no_order['id']==0)]
+    return no_order_f[["Customers"]]
