02a394a645ef9f8cc1443eeddc7b2dcd46ab73fc
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-08-13 20:56:47 +0530

    Create Q1667 - Fix Names in a Table.py

---
+++ pandas-course/Q1667 - Fix Names in a Table.py
@@ -0,0 +1,22 @@
+#Question:
+# Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+# Return the result table ordered by user_id.
+
+# Input: 
+# Users table:
+# +---------+-------+
+# | user_id | name  |
+# +---------+-------+
+# | 1       | aLice |
+# | 2       | bOB   |
+# +---------+-------+
+
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users.name = users.name.str.capitalize()
+
+    result = users.sort_values(by='user_id', ascending=True)
+
+    return result
