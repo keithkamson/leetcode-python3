8ee9a5d27268916cd99c15e5be12fea4db020ea7
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-08-15 13:51:52 +0530

    Q1527

---
+++ pandas-course/Q1527 - Patients With a Condition.py
@@ -0,0 +1,23 @@
+#Question
+# Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+# Return the result table in any order.
+
+# Input: 
+# Patients table:
+# +------------+--------------+--------------+
+# | patient_id | patient_name | conditions   |
+# +------------+--------------+--------------+
+# | 1          | Daniel       | YFEV COUGH   |
+# | 2          | Alice        |              |
+# | 3          | Bob          | DIAB100 MYOP |
+# | 4          | George       | ACNE DIAB100 |
+# | 5          | Alain        | DIAB201      |
+# +------------+--------------+--------------+
+
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    condition = patients[patients.conditions.str.contains(r'\bDIAB1')]
+    result = condition[['patient_id','patient_name', 'conditions']]
+    return result
