e6ed1fbd71fea6155e052832e6a18e73bfe36318
Keith Kamson Fernandes <keithkamson@gmail.com>
2023-08-16 22:39:12 +0530

    Create Q196 - Delete Duplicate Emails.py

---
+++ pandas-course/Q196 - Delete Duplicate Emails.py
@@ -0,0 +1,24 @@
+#Question:
+#Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+# For Pandas users, please note that you are supposed to modify Person in place.
+
+# After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+
+# Input: 
+# Person table:
+# +----+------------------+
+# | id | email            |
+# +----+------------------+
+# | 1  | john@example.com |
+# | 2  | bob@example.com  |
+# | 3  | john@example.com |
+# +----+------------------+
+
+
+import pandas as pd
+
+# Modify Person in place
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by = 'id', ascending = True, inplace = True)
+    person.drop_duplicates(subset='email', keep = 'first', inplace = True)
